499138e3d92e1fe027fb062a4204241315217324
ta.kiyama <ta.kiyama@eimee.co.jp>
2018-06-29 15:52:21 +0900

    v5.3.3

--- src/utils/stepAsync.js
+++ src/utils/stepAsync.js
@@ -4,7 +4,7 @@ import generateEffectProps from "../generateEffectProps";
 const stepAsync = async (value) => {
   value = (value instanceof Promise) ? (await value).value : value;
 
-  const [nextArg, options] = execNextArg(value);
+  let [nextArg, options] = execNextArg(value);
 
   if(options.isAsync) nextArg = await nextArg;
   if(options.isError) throw nextArg;
